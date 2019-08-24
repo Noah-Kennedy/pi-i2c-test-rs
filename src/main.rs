a4510cbbedba6fb6c7de20be0a39a39eab0af824
noah <nomaxx117@gmail.com>
2019-08-24 14:15:11 -0500

    Moviong to other lib

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use rppal::i2c;
+use i2cdev::linux::LinuxI2CDevice;
+use i2cdev::core::I2CDevice;
 
 const I2C_TX_MESSAGE: [u8; 4] = [0xde, 0xad, 0xbe, 0xef];
 
 fn main() {
-    let mut bus = i2c::I2c::new().unwrap();
+    let mut bus = LinuxI2CDevice::new("/dev/i2c-1", 7).unwrap();
 
     bus.set_slave_address(7).unwrap();
 
     let mut rx_buffer: [u8; 32] = [0; 32];
 
     loop {
-        let tx_length = bus.write(&I2C_TX_MESSAGE).unwrap();
+        bus.write(&I2C_TX_MESSAGE).unwrap();
         println!("TX: {:x?}", I2C_TX_MESSAGE);
-        println!("Wrote {} bytes", tx_length);
 
-        let rx_length = bus.read(&mut rx_buffer).unwrap();
+        bus.read(&mut rx_buffer).unwrap();
         println!("RX: {:x?}", rx_buffer);
-        println!("Read {} bytes", rx_length);
     }
 }
